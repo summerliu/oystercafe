b696d60f019984ff9a2ed065c2047a8d8f43b1cd
summerliu <summer.ycliu@gmail.com>
2020-02-03 12:43:02 +0800

    Updates

---
+++ precache-manifest.f09fb667ebb8fc33c00ae22df6e5dd0a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f31fa57614ff2fae57a33fdac294cde9",
+    "url": "/oystercafe/index.html"
+  },
+  {
+    "revision": "74a50b4735e24155fc73",
+    "url": "/oystercafe/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ad2b7e33da73c69b8fb9",
+    "url": "/oystercafe/static/js/2.ea211f39.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/oystercafe/static/js/2.ea211f39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74a50b4735e24155fc73",
+    "url": "/oystercafe/static/js/main.715c8640.chunk.js"
+  },
+  {
+    "revision": "204f171c4abdd7b7d640",
+    "url": "/oystercafe/static/js/runtime-main.8009b551.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/oystercafe/static/media/logo.5d5d9eef.svg"
+  }
+]);
